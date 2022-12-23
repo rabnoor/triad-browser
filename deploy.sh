ceba93c7a134762a46a6b7ece8c4e70af6b3cb56
kiranbandi <bvenkatkiran@gmail.com>
2022-12-22 18:43:29 -0600

    force install in deploy script due to outdated package support

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-npm install
-npm run build
+npm install -f
+NODE_OPTIONS=--openssl-legacy-provider npm run build
 rm -rf /var/www/triad-browser/*
 cp -r dist/. /var/www/triad-browser
 systemctl reload nginx
